65ca807d62ef39047b9ff25b8e7b7b9116bbc0f7
qqq000a@outlook.com <78515568+qq8244353@users.noreply.github.com>
2022-05-18 15:08:01 +0900

    tsでHelloWorld

---
+++ helloworld.ts
@@ -0,0 +1,9 @@
+class HelloWorld {
+	constructor(public displayText : string) {}
+	greet() {
+		return this.displayText;
+	}
+}
+
+var helloWorld = new HelloWorld("HelloWorld");
+document.body.innerHTML = helloWorld.greet();
